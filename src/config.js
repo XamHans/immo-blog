625907b0381341578ac148f33d41435ae00e6456
Johannes <muellerjohannes93@gmail.com>
2020-07-02 21:23:31 +0200

    yuhu

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "MDBhNDgxN2U2MTcxZmFlMjJlZmJkYzcyYTRiMzFjYjAwMjI1OTZlMiA=",
+
+  // Your Github UserName
+  githubUserName: "XamHans",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "immo-blog",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'bbf87ecb92e6db9e94e4',
+  OAuthSecret: '77346ebf4e28ba24ba1622a755f62bca38c5f89c',
+
+  // Your Personal Blog Title
+  title : "ImmoRobo Blog" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Wohnungen - Häuser - Immobilien",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
