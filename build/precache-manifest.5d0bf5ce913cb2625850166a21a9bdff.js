b0be152913320a1543ce9f086f6ae4d058b7be42
Johannes <muellerjohannes93@gmail.com>
2020-07-26 14:07:14 +0200

    build

---
+++ build/precache-manifest.5d0bf5ce913cb2625850166a21a9bdff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbcd6c502911359de60195b771de7121",
+    "url": "/immo-blog/index.html"
+  },
+  {
+    "revision": "2ce9c3b25296bc48952e",
+    "url": "/immo-blog/static/css/main.591e532c.chunk.css"
+  },
+  {
+    "revision": "83cdeb99fae9b7254742",
+    "url": "/immo-blog/static/js/2.d674b4be.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/immo-blog/static/js/2.d674b4be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ce9c3b25296bc48952e",
+    "url": "/immo-blog/static/js/main.f8d37112.chunk.js"
+  },
+  {
+    "revision": "71de0548884d8d2a0a85",
+    "url": "/immo-blog/static/js/runtime-main.393b02c7.js"
+  }
+]);
